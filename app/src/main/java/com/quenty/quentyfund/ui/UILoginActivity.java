52c0bec3552fef9ce853481938955fa93055d8a4
luisdavorl@hotmail.com <ldavorla1>
2015-10-20 22:51:32 -0400

    Some Changes on intruduction and login... only to perform some tips

--- app/src/main/java/com/quenty/quentyfund/ui/UILoginActivity.java
+++ app/src/main/java/com/quenty/quentyfund/ui/UILoginActivity.java
@@ -1,12 +1,15 @@
 package com.quenty.quentyfund.ui;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +27,7 @@ import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.ProfileTracker;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.common.ConnectionResult;
@@ -35,125 +39,115 @@ import com.google.android.gms.plus.Plus;
 import com.google.android.gms.plus.model.people.Person;
 import com.quenty.quentyfund.R;
 import com.quenty.quentyfund.bll.BLLUser;
+import com.quenty.quentyfund.entity.User;
 
 import org.json.JSONObject;
 
+import java.util.Arrays;
 import java.util.Set;
 
 public class UILoginActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        GoogleApiClient.ServerAuthCodeCallbacks {
+        GoogleApiClient.OnConnectionFailedListener {
 
-    //    Google+
-    private GoogleApiClient mGoogleApiClient;
     private static final int RC_SIGN_IN = 0;
-    private boolean banActive = false;
-//    private static final String WEB_CLIENT_ID = "844991406801-psilhe0c6rqgp1urvjnkp22eusthirli.apps.googleusercontent.com";
-
-    // Base URL for your token exchange server, no trailing slash.
-//    private static final String SERVER_BASE_URL = "SERVER_BASE_URL";
-    //    Facebook
-    LoginButton loginButton;
+    private ProgressDialog progressDialog;
+    private GoogleApiClient plusClient;
     private CallbackManager callbackManager;
-    private AccessTokenTracker accessTokenTracker;
-    private ProfileTracker profileTracker;
-
-
-    private FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
-        @Override
-        public void onSuccess(LoginResult loginResult) {
-//            AccessToken accessToken = loginResult.getAccessToken();
-//            Profile profile = Profile.getCurrentProfile();
-//            if (profile != null) {
-//                Log.i("profile", profile.getName());
-////                BLLUser bllUser = new BLLUser(getBaseContext());
-////                bllUser.insert(profile.getFirstName(), profile.getLastName(), profile.getId());
-////                startActivity(new Intent(UILoginActivity.this, UIWelcomeActivity.class));
-////                UILoginActivity.this.finish();
-////            displayMessage(profile);
-//            }
-            GraphRequest request = GraphRequest.newMeRequest(
-                    loginResult.getAccessToken(),
-                    new GraphRequest.GraphJSONObjectCallback() {
-                        @Override
-                        public void onCompleted(
-                                JSONObject object,
-                                GraphResponse response) {
-                            // Application code
-                            Log.v("LoginActivity", response.toString());
-                        }
-                    });
-            Bundle parameters = new Bundle();
-            parameters.putString("fields", "id,name,email,gender, birthday");
-            request.setParameters(parameters);
-            request.executeAsync();
-        }
-
-        @Override
-        public void onCancel() {
-
-        }
-
-        @Override
-        public void onError(FacebookException e) {
-
-        }
-    };
+    private boolean mIntentInProgress;
+    private LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
-        loginButton = (LoginButton) findViewById(R.id.login_button);
 
-        loginButton.setReadPermissions("public_profile, email, user_birthday, user_friends");
-//        loginButton.setFragment(this);
-        callbackManager = CallbackManager.Factory.create();
-
-        loginButton.registerCallback(callbackManager, callback);
-        accessTokenTracker = new AccessTokenTracker() {
-            @Override
-            protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
-
-            }
-        };
-
-        profileTracker = new ProfileTracker() {
-            @Override
-            protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
-                if (newProfile != null) {
-                    Log.i("newProfile", newProfile.toString());
-                    BLLUser bllUser = new BLLUser(getBaseContext());
-                    bllUser.insert(newProfile.getFirstName(), newProfile.getLastName(), newProfile.getId());
-                    startActivity(new Intent(UILoginActivity.this, UIWelcomeActivity.class));
-                    UILoginActivity.this.finish();
-                }
-            }
-        };
-
-        accessTokenTracker.startTracking();
-        profileTracker.startTracking();
-
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
+        plusClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(Plus.API)
                 .addScope(Plus.SCOPE_PLUS_LOGIN)
                 .build();
+
+
+        if (plusClient.isConnected()) {
+            Plus.AccountApi.clearDefaultAccount(plusClient);
+            plusClient.disconnect();
+            plusClient.connect();
+        }
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Conectando...");
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        loginButton.setReadPermissions("public_profile, email, user_birthday, user_friends");
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        try {
+//                            TelephonyManager tMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+//                            String mPhoneNumber = tMgr.getLine1Number();
+
+                            String id = object.getString("id");
+                            String personName = object.getString("name");
+
+//                            User user = new User();
+//                            user.setId(0L);
+//                            user.setGcm_id(id);
+//                            user.setNroTelefono(mPhoneNumber);
+//                            user.setNombres(personName);
+//                            user.setId_facebook(id);
+//                            BLLUser bllUser = new BLLUser(getBaseContext());
+//                            bllUser.insert(newProfile.getFirstName(), newProfile.getLastName(), newProfile.getId());
+                            startActivity(new Intent(UILoginActivity.this, UIWelcomeActivity.class));
+                            UILoginActivity.this.finish();
+
+                        } catch (Exception e) {
+                            LoginManager.getInstance().logOut();
+//                            new Utils(LoginActivity.this).writeUser(null);
+                            e.printStackTrace();
+                        }
+                    }
+                }).executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i("Facebook", "Login cancelado");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                exception.printStackTrace();
+            }
+        });
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginManager.getInstance().logInWithReadPermissions(UILoginActivity.this, Arrays.asList("public_profile, email, user_birthday, user_friends"));
+            }
+        });
+
+
         SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
 
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                mGoogleApiClient.connect();
-
+                plusClient.connect();
+                progressDialog.show();
             }
         });
+
     }
 
 
@@ -161,79 +155,39 @@ public class UILoginActivity extends AppCompatActivity implements
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == RC_SIGN_IN) {
-//            mIntentInProgress = false;
-
-            if (!mGoogleApiClient.isConnecting()) {
-                mGoogleApiClient.connect();
-            }
-        } else {
+            mIntentInProgress = false;
+        }
+        if (callbackManager != null)
             callbackManager.onActivityResult(requestCode, resultCode, data);
-
-        }
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        accessTokenTracker.stopTracking();
-        profileTracker.stopTracking();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-//        if (mGoogleApiClient.isConnected()) {
-
-//            Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
-
-//            mGoogleApiClient.disconnect();
-//            mGoogleApiClient.connect();
-//        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-//        AppEventsLogger.deactivateApp(this);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Profile profile = Profile.getCurrentProfile();
-        if (profile != null) {
-            Log.i("Resumen", profile.toString());
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-//        mGoogleApiClient.connect();
-    }
 
     @Override
     public void onConnected(Bundle bundle) {
-        banActive = true;
-        Log.i("Conectado", " - ");
-        if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
-            Person currentPerson = Plus.PeopleApi
-                    .getCurrentPerson(mGoogleApiClient);
+
+        progressDialog.dismiss();
+        if (Plus.PeopleApi.getCurrentPerson(plusClient) != null) {
+            TelephonyManager tMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+            String mPhoneNumber = tMgr.getLine1Number();
+
+            Person currentPerson = Plus.PeopleApi.getCurrentPerson(plusClient);
             String personName = currentPerson.getDisplayName();
-            String personPhotoUrl = currentPerson.getImage().getUrl();
-            String personGooglePlusProfile = currentPerson.getUrl();
-            String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
-            Log.i("Google +", personName + " - " + email);
-            BLLUser bllUser = new BLLUser(getBaseContext());
-            bllUser.insert(personName, "", email);
+            String personPhoto = currentPerson.getImage().getUrl();
+            String personId = currentPerson.getId();
+            String personEmail = currentPerson.getUrl();
+
+//            User user = new User();
+//            user.setId(0L);
+//            user.setGcm_id(personId);
+//            user.setEmail(personEmail);
+//            user.setNroTelefono(mPhoneNumber);
+//            user.setNombres(personName);
+//            user.setId_google(personPhoto);
+//            BLLUser bllUser = new BLLUser(getBaseContext());
+//            bllUser.insert(personName, "", email);
             startActivity(new Intent(UILoginActivity.this, UIWelcomeActivity.class));
             UILoginActivity.this.finish();
-
         }
-//        Person currentUser = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
-////        Person signedInUser = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
-//        String userEmail = Plus.AccountApi.getAccountName(mGoogleApiClient);
-//        Log.i("Google +", currentUser.getName() + " - " + userEmail);
     }
 
     @Override
@@ -243,27 +197,20 @@ public class UILoginActivity extends AppCompatActivity implements
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.i("onConnectionFailed", " - " + connectionResult.toString());
-        if (banActive) {
-            try {
-                startIntentSenderForResult(connectionResult.getResolution().getIntentSender(), RC_SIGN_IN, null, 0, 0, 0);
-            } catch (IntentSender.SendIntentException e) {
-                e.printStackTrace();
+        try {
+            progressDialog.dismiss();
+            if (!mIntentInProgress && connectionResult.hasResolution()) {
+                try {
+                    mIntentInProgress = true;
+                    startIntentSenderForResult(connectionResult.getResolution().getIntentSender(),
+                            RC_SIGN_IN, null, 0, 0, 0);
+                } catch (IntentSender.SendIntentException e) {
+                    mIntentInProgress = false;
+                }
             }
-            banActive = false;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
     }
 
-    @Override
-    public CheckResult onCheckServerAuthorization(String s, Set<Scope> set) {
-        Log.i("onCheckServerAuthorization", " - ");
-        return null;
-    }
-
-    @Override
-    public boolean onUploadServerAuthCode(String s, String s1) {
-        Log.i("onUploadServerAuthCode", " - ");
-        return false;
-    }
 }
